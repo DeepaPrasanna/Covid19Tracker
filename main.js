2bfc2545ae1d1783a87cdea87684f9e0ed96b51f
DeepaPrasanna <anneraj73@gmail.com>
2020-04-16 11:53:33 +0530

    retreive data from API

--- main.js
+++ main.js
@@ -11,12 +11,55 @@ let state = document.getElementById("state")
 //             console.log(arr[i].state + "\n" + arr[i].confirmed);
 //         }
 //     })
-//     .catch(error => {
-//         console.error(error);
-//     });
-coronaData();
+//     
+const API_URL = 'https://api.covid19india.org/data.json'
+// let ul = document.querySelector(".list-items")
+let div = document.querySelector(".table-items")
+
 async function coronaData() {
-    const response = await fetch('https://api.covid19india.org/data.json');
+    const response = await fetch(API_URL);
     const data = await response.json();
-    console.log(data.statewise);
+    // console.log(data.statewise); 
+    for (let i = 0; i < data.statewise.length; i++) {
+        const { active, confirmed, deaths, state } = data.statewise[i];
+        // console.log(active)
+        // console.log(confirmed)
+        // console.log(deaths)
+        // console.log(state)
+        // document.getElementById("state").textContent = state;
+        // document.getElementById("confirmed").textContent = confirmed;
+        // document.getElementById("deaths").textContent = deaths;
+
+        // document.getElementById("active").textContent = active;
+        let ul = document.createElement("ul")
+        ul.classList.add("list-items")
+        ul.classList.add("horizontal-list")
+
+        let stateItem = document.createElement("li");
+        stateItem.textContent = state;
+        ul.appendChild(stateItem);
+
+        let activeItem = document.createElement("li");
+        activeItem.textContent = active;
+        ul.appendChild(activeItem);
+
+        let confirmedItem = document.createElement("li")
+        confirmedItem.textContent = confirmed;
+        ul.appendChild(confirmedItem);
+
+        let deathItem = document.createElement("li");
+        deathItem.textContent = deaths;
+        ul.appendChild(deathItem);
+
+        div.appendChild(ul);
+
+
+
+
+
+    }
+
 }
+coronaData().catch(error => {
+    console.error(error);
+});;
